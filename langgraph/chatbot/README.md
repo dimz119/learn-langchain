d12b0feb049e9244cda9b5cc494cec81bc9c5d5e
Seungjoon Lee <joonlee0228@gmail.com>
2025-04-08 21:04:28 -0700

    added LangGraph

---
+++ langgraph/chatbot/README.md
@@ -0,0 +1,10 @@
+# LangGraph
+
+LangGraph is a powerful tool developed by the LangChain team to simplify the creation and visualization of complex workflows in natural language processing (NLP) applications. It provides an intuitive interface for designing, debugging, and optimizing workflows, making it easier to build robust and scalable applications.
+
+## Features
+
+- **Workflow Visualization**: Create and visualize complex workflows with ease.
+- **Debugging Tools**: Identify and resolve issues in your workflows efficiently.
+- **Integration with LangChain**: Seamlessly integrates with the LangChain framework for enhanced functionality.
+- **Scalability**: Build workflows that scale with your application's needs.
---
+++ langgraph/chatbot/simple_chatbot.py
@@ -0,0 +1,51 @@
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain_openai import ChatOpenAI
+
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+llm = ChatOpenAI(model="gpt-4o") # type: ignore
+
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+
+# The first argument is the unique node name
+# The second argument is the function or object that will be called whenever
+# the node is used.
+graph_builder.add_node("chatbot", chatbot)
+
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+graph = graph_builder.compile()
+# print(graph.get_graph().draw_mermaid())
+# https://mermaid.live/
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+        stream_graph_updates(user_input)
+    except:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
